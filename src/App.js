a49b8b70a65b7fe0b8cf8a5cba8c36f70db3944b
HannesEnnemann <hannesthe1@proton.me>
2023-11-28 09:24:14 +1100

    bold main title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="heading-contain">
+        <h1>
+          <span className="title">Gorilla</span>
+          <span className="title-extension">.ai</span>
+        </h1>
+      </div>
     </div>
   );
 }

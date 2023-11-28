df18708f050e20b634be4326de0b59ad3bdfbf7b
HannesEnnemann <hannesthe1@proton.me>
2023-11-28 14:06:25 +1100

    product and company page initial

---
+++ src/Company.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Company.css";
+
+export default function Company() {
+  return (
+    <div>
+      <h1>Company</h1>
+    </div>
+  );
+}
---
+++ src/Products.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Products.css";
+
+export default function Products() {
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
+}

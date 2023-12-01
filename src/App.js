e1a0620ec94354a787823583508eef75cbfb039b
HannesEnnemann <hannesthe1@proton.me>
2023-12-01 14:01:16 +1100

    routing functional

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Products from "./Products";
 import Company from "./Company";
@@ -7,11 +7,11 @@ import Company from "./Company";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/products" component={Products} />
-        <Route path="/company" component={Company} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/company" element={<Company />} />
+      </Routes>
     </Router>
   );
 }

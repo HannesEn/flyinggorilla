303610c0bb5bb4da8309dcbee3029ed06630934f
HannesEnnemann <hannesthe1@proton.me>
2023-12-01 15:11:36 +1100

    auth(redirect url broken)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import Home from "./Home";
 import Products from "./Products";
 import Company from "./Company";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/company" element={<Company />} />
-      </Routes>
+      <KindeProvider
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        domain={process.env.REACT_APP_DOMAIN}
+        redirectUri={process.env.REACT_APP_REDIRECT_URI}
+        logoutUri={process.env.REACT_APP_LOGOUT_URI}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </KindeProvider>
     </Router>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+export default function Login() {
+  const { login, register } = useKindeAuth();
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Welcome, how would you like to continue?</h1>
+      <div>
+        <button className="register-button" onClick={register} type="button">
+          Register
+        </button>
+      </div>
+      <div>
+        <button className="login-button" onClick={login} type="button">
+          Log In
+        </button>
+      </div>
+      <div>
+        <button className="back-button" onClick={goBack} type="button">
+          Back
+        </button>
+      </div>
+    </div>
+  );
+}

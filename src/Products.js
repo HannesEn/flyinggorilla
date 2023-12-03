7a2548ab0c0f1f1de250ee4d220aa3d3c3365614
HannesEnnemann <hannesthe1@proton.me>
2023-12-03 12:42:18 +1100

    products page(basic)

--- src/Products.js
+++ src/Products.js
@@ -6,7 +6,20 @@ export default function Products() {
   return (
     <div>
       <Nav activeLink={"products"} />
-      <h1>Products</h1>
+      <div className="products-container">
+        <h1 className="products-title">Our Products</h1>
+        <h2 className="products-sub-heading-1">
+          <span className="products-nr-1">01 </span>
+          <span className="products-title-span-1"> Audio</span>
+        </h2>
+        <p>
+          We are currently working on a new audio product that will be released
+          in 2024.
+        </p>
+        <button className="explore-products-button" type="button">
+          Explore
+        </button>
+      </div>
     </div>
   );
 }

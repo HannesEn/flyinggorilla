f2e10180b35acb4d12496c88c5924c50a6a85a3e
HannesEnnemann <hannesthe1@proton.me>
2023-12-17 12:39:57 +1100

    Fixed relative positioning for "Flying" and "Gorilla" on home page. Now adapts on any window size.

--- src/Home.js
+++ src/Home.js
@@ -24,11 +24,15 @@ function Home() {
       <div className="body">
         <header className="App-header"></header>
         <div className="heading-contain">
-          <h1 className="small-title">Flying</h1>
-          <h1 className="main-title-contain">
-            <span className="title">Gorilla</span>
-            <span className="title-extension">.ai</span>
-          </h1>
+          <div className="title-container">
+            {" "}
+            {/* Add this wrapper */}
+            <h1 className="small-title">Flying</h1>
+            <h1 className="main-title-contain">
+              <span className="title">Gorilla</span>
+              <span className="title-extension">.ai</span>
+            </h1>
+          </div>
         </div>
       </div>
     </div>
